2da90df386e3948199813ed7b3c4f4495d8d8d1a
Isabelle Cristina <110576073+isabellecristinav@users.noreply.github.com>
2022-11-30 22:17:53 -0300

    continuação

---
+++ 022/app.js
@@ -0,0 +1,15 @@
+function MaiorNumero(){
+    let n1 = document.getElementById('valor1').value
+    let n2 = document.getElementById('valor2').value
+    let n3 = document.getElementById('valor3').value
+
+    if(n1 > n2 && n1> n3){
+        document.getElementById('resultado').innerHTML = "O maior número é: " + n1
+    }else if(n2 > n1 && n2 > n3){
+        document.getElementById('resultado').innerHTML = "O maior número é: " + n2
+    }
+    else{
+        document.getElementById('resultado').innerHTML = "O maior número é: " + n3
+    }
+
+}
