dda9a0952466a4f4758106c043eac1a8671d51bb
Isabelle Cristina <110576073+isabellecristinav@users.noreply.github.com>
2022-12-01 22:22:06 -0300

    continuação das atividades

---
+++ 015/app.js
@@ -0,0 +1,5 @@
+function Area(){
+    let area = document.getElementById('area').value
+
+    document.getElementById('resultado').innerHTML = area * Math.PI 
+}
---
+++ 026/app.js
@@ -0,0 +1,3 @@
+function Turno{
+    let
+}
